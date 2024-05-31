4f65fec542e8d46cf509e07ab0d96785bee6c98e
julianasoto5 <115961804+julianasoto5@users.noreply.github.com>
2024-05-30 21:30:33 -0300

    Update Ejercicio5.cs

--- Practica4/Ejercicio5.cs
+++ Practica4/Ejercicio5.cs
@@ -1,1 +1,8 @@
-
+/**
+Resolver la administracion de las impresoras de una oficina. Hay 3 impresoras, 
+N usuarios y 1 director. Los usuarios y el director están continuamente trabajando
+y cada tanto envían documentos a imprimir. Cada impresora, cuando está libre, toma 
+un documento y lo imprime, de acuerdo al orden de llegada, pero siempre dando
+prioridad a los pedidos del director. Nota: los usuarios y el director no deben 
+esperar a que se imprima el documento.
+**/
