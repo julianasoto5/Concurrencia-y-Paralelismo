3bf210b1ed7b3632cd361474c596967e5fa2a935
julianasoto5 <115961804+julianasoto5@users.noreply.github.com>
2024-05-31 13:28:30 -0300

    Update Ejercicio5.cs

--- Practica4/Ejercicio5.cs
+++ Practica4/Ejercicio5.cs
@@ -6,3 +6,61 @@ un documento y lo imprime, de acuerdo al orden de llegada, pero siempre dando
 prioridad a los pedidos del director. Nota: los usuarios y el director no deben 
 esperar a que se imprima el documento.
 **/
+
+chan listaImpresora[3] (texto);
+chan DocumentosDirector(texto);
+chan DocumentosUsuarios(texto);
+chan Aviso(int);
+
+Process Impresora[id: 0..2]
+{ int ready;
+  texto doc;
+  while (true){
+    receive listaImpresora[id](doc);
+    Imprimir(doc);
+  }
+}
+
+Process Usuario[id: 0..N-1]{
+  while (true){
+      while (noNecesiteImprimir());
+      doc = documento_a_imprimir();
+      send DocumentosUsuarios(doc);
+      send Aviso(1);
+  }
+  
+}
+
+Process Director{
+  while (true){
+    while (noNecesiteImprimir());
+    doc = documento_a_imprimir();
+    send DocumentosDirector(doc);
+    send Aviso(1);
+  }
+}
+
+
+Process Coordinador //coordina entre las tres impresoras. Si alguna esta libre, le manda el documento
+{ int ok;
+  while (true){
+    receive Aviso(ok); //alguien quiere imprimir
+
+    //chequeamos quien quiere imprimir de acuerdo a la prioridad D > U
+    if (not empty(DocumentosDirector)) ->
+      receive DocumentosDirector(doc);
+    [] if ((not empty(DocumentosUsuarios)) and (empty(DocumentosDirector)))->
+          receive DocumentosUsuarios(doc);
+    fi
+    
+    //chequeamos cual impresora esta vacia y le mandamos el documento
+    if (empty (listaImpresora[0]))->
+        send listaImpresora[0](doc);
+    [] if (empty (listaImpresora[1]))->
+        send listaImpresora[1](doc);
+    [] if (empty (listaImpresora[2]))->
+        send listaImpresora[2](doc);
+    fi
+    
+  }
+}
