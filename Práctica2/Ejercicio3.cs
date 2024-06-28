0400f4b27d2edff88f62406e2c896782eafd806e
julianasoto5 <115961804+julianasoto5@users.noreply.github.com>
2024-06-28 01:29:34 -0300

    Create Ejercicio3.cs

---
+++ Práctica2/Ejercicio3.cs
@@ -0,0 +1,7 @@
+/*
+Se tiene un curso con 40 alumnos. La maestra entrega una tarea distinta a cada alumno, 
+luego cada alumno realiza su tarea y se la entrega a la maestra para que la corrija. Esta
+revisa la tarea y si está bien le avisa al alumno que puede irse. Si la tarea está mal le
+indica los errores y volverá a entregarle la tarea a la maestra para que realice la 
+corrección nuevamente. Esto se repite hasta que la tarea no tenga errores.
+*/
