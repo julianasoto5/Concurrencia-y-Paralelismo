283444d59d1a637e79ddde01f499f3ce46ec81ff
julianasoto5 <115961804+julianasoto5@users.noreply.github.com>
2024-07-10 23:02:49 -0300

    Update Ejercicio1.cs

--- Práctica3/Ejercicio1.cs
+++ Práctica3/Ejercicio1.cs
@@ -1,2 +1,37 @@
 /*Implementar el acceso a una base de datos de solo lectura que puede 
 atender a lo sumo 5 consultas simultáneas*/
+
+//NO ESPECIFICA ORDEN
+Monitor BD{
+  int cantC = 5;
+  cond espera;
+  
+  Procedure readRQ(id: in int){
+    if (cantC == 0){ //no esta disponible
+      wait(espera);
+    }
+    //esta disponible (cantC > 0 o salio del wait)  
+    cantC--;
+  }
+
+  Procedure Salir(){
+    cantC++;
+    signal(cola);
+  }
+  
+}
+
+Process Usuario[id:0..U-1]
+{
+  while(true){
+    BD.readRQ();
+    hacerConsulta();
+    BD.Salir();
+  }
+  
+}
+
+Process Admin{
+  
+}
+
