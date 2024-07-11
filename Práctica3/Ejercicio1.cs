4eb68f7ec101e44224ee5c5ea68ec00d2c90e507
julianasoto5 <115961804+julianasoto5@users.noreply.github.com>
2024-07-10 23:14:09 -0300

    Update Ejercicio1.cs

--- Práctica3/Ejercicio1.cs
+++ Práctica3/Ejercicio1.cs
@@ -3,20 +3,23 @@ atender a lo sumo 5 consultas simultáneas*/
 
 //NO ESPECIFICA ORDEN
 Monitor BD{
-  int cantC = 5;
+  int cantC = 5, esperando = 0;
   cond espera;
   
   Procedure readRQ(id: in int){
     if (cantC == 0){ //no esta disponible
       wait(espera);
+      esperando++;
     }
     //esta disponible (cantC > 0 o salio del wait)  
     cantC--;
   }
 
   Procedure Salir(){
-    cantC++;
-    signal(cola);
+    if (esperando > 0) {
+      esperando--;
+      signal(cola);
+    }else cantC++; 
   }
   
 }
