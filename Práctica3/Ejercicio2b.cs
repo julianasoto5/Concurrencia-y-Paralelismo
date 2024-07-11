0370d70ee2792c08be85e9b18382f373bf83d9c0
julianasoto5 <115961804+julianasoto5@users.noreply.github.com>
2024-07-10 23:23:59 -0300

    Update Ejercicio2b.cs

--- Práctica3/Ejercicio2b.cs
+++ Práctica3/Ejercicio2b.cs
@@ -6,13 +6,26 @@ b) Modifique la solución de (a) para el caso en que se deba respetar el orden
 */
 
 Monitor Fotocopiadora{
+  cond esperar;
+  boolean libre = true;
+  int esperando = 0;
+  
   Procedure Usar(){
-    
+    if (not libre){
+      esperando++;
+      wait(esperar);
+    }
+    else libre = false; //si no estaba marcado como ocupado ahora sí lo está
   }
-  Procedure Salir(){
-    
+  Procedure Salir(){ //hay que darle "prioridad" a quien ya esta en la cola. Además se pierde el EM
+    if (esperando == 0)  libre = true;
+    else{ 
+      esperando--;
+      signal(esperar);
+    }
   }
 }
+
 
 Process Persona[id:0..N-1]
 {
@@ -20,3 +33,4 @@ Process Persona[id:0..N-1]
   Fotocopiar();
   Fotocopiadora.Salir();
 }
+
