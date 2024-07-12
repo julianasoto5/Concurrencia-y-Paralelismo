0b5fcb86e66dceb11eeb486e50dda3096e8dcc75
julianasoto5 <115961804+julianasoto5@users.noreply.github.com>
2024-07-11 22:35:44 -0300

    Create Ejercicio2.cs

---
+++ Practica1/Ejercicio2.cs
@@ -0,0 +1,18 @@
+/*
+Realice una solución de grano grueso (utilizando <> y/o <await B; S>) 
+para el siguiente problema. Un sistema operativo mantiene 5 instancias 
+de un recurso almacenadas en una cola, cuando un proceso necesita usar
+una instancia del recurso la saca de la cola, la usa y cuando termina
+de usarla la vuelve a depositar.
+*/
+
+cola buffer;
+int s = 5;
+Process Proceso[id:0..P-1]
+{ int idI;
+  <await s > 0; s=s-1>>; //equivalente a P(s);
+  <pop(buffer,idI)>;
+  usarInstancia(idI);
+  <s=s+1>; //
+  
+}
